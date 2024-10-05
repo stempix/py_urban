17d20bba73415be6268454c9804e8648e3d66947
Minnulin Emil <e.r.minnulin@yandex.ru>
2024-10-05 14:38:45 +0500

    Base data structures homework completing

---
+++ homework/base_data_structures/task3.py
@@ -0,0 +1,6 @@
+# 3rd program
+first = 2 * 2 + 2
+second = 2 * (2 + 2)
+print(first)
+print(second)
+print(first == second)
---
+++ homework/base_data_structures/task4.py
@@ -0,0 +1,5 @@
+# 4th program
+text = '123.456'
+number = float(text)
+intNumber = int(number * 10)
+print(intNumber % 10)
