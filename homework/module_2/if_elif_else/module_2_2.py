8cac2353aed8563d6f6c423329ad00a7a6449f2b
Minnulin Emil <e.r.minnulin@yandex.ru>
2024-10-13 15:02:29 +0500

    If, elif and else homework completion

---
+++ homework/module_2/if_elif_else/module_2_2.py
@@ -0,0 +1,12 @@
+# Считываем числа
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+
+# Сравниваем числа
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
