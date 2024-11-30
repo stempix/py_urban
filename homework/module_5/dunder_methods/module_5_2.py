7dc27986430b9b054c83720984cebd3ac8cdfc89
Minnulin Emil <e.r.minnulin@yandex.ru>
2024-11-30 21:15:16 +0500

    Operator overloading homework completion

--- homework/module_5/dunder_methods/module_5_2.py
+++ homework/module_5/dunder_methods/module_5_2.py
@@ -6,7 +6,7 @@ class House:
         self.number_of_floors = _floors_num
 
     def go_to(self, new_floor):
-        if new_floor > self.number_of_floors:
+        if new_floor < 1 or new_floor > self.number_of_floors:
             print("Такого этажа не существует")
         else:
             for i in range(1, new_floor + 1):
