26bfe71969b501e0b467e48a85a486d3d83417a1
Minnulin Emil <e.r.minnulin@yandex.ru>
2024-12-09 19:41:14 +0500

    Files open modes homework completion

---
+++ homework/module_7/files_open_modes/module_7_1.py
@@ -0,0 +1,41 @@
+from sysconfig import get_path
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+class Shop:
+    __file_name = "products.txt"
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+        return products
+
+    def add(self, *products):
+        existing_products = self.get_products()
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if product.name not in existing_products:
+                file.write(str(product) + "\n")
+            else:
+                print(f"Продукт {product.name} уже есть в магазине")
+        file.close()
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
